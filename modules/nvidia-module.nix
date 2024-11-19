6dc52bc02a250b84b7e13ae8c48cc49ab840b9af
= <aaron.karlsen@gmail.com>
2024-11-19 22:05:28 +1000

    fixed nvidia module

--- modules/nvidia-module.nix
+++ modules/nvidia-module.nix
@@ -1,13 +1,15 @@
-{ lib, pkgs, config, ... }:
-with lib;
+{ config, lib, pkgs, ... }:
 {
+  hardware.opengl = {
+    enable = true;
+    };
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    hardware.nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = false;
+      powerManagement.finegrained = false;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
 }
